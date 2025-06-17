c3e4f410e1e05458c354c967f0ba9b9bf51756bc
Bogdan0759 <Bogdan4ik0759@gmail.com>
2025-06-17 20:27:17 +0300

    Добавление цикла и выхода по exit в калькулятор

--- programs/calculator.py
+++ programs/calculator.py
@@ -1,9 +1,13 @@
 def run_calculator():
     print("\n--- Калькулятор ---")
-    expression = input("Введите математическое выражение (например, 2+2*3): ")
-    try:
-        result = eval(expression)
-        print(f"Результат: {result}")
-    except Exception as e:
-        print(f"Ошибка при вычислении: {e}")
+    print("Введите математическое выражение (например, 2+2*3) или 'exit' для выхода.")
+    while True:
+        expression = input("> ")
+        if expression.lower() == 'exit':
+            break
+        try:
+            result = eval(expression)
+            print(f"Результат: {result}")
+        except Exception as e:
+            print(f"Ошибка при вычислении: {e}")
     print("---------------------\n")
